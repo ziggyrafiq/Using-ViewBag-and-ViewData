682e1453e95f8667960eea0a06e9c42f81d36bfc
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2024-03-17 15:34:40 +0000

    Using ViewBag and ViewData

---
+++ Code Example/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Code_Example.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Code_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "I am Ziggy Rafiq";
+            ViewData["Message"] = "This is Ziggy Rafiq Blog Site";
+            return View();
+        }
+        public ActionResult GetData(string myData)
+        {
+            return Content(myData);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Code Example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Code_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
